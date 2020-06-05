2f9501e1556f12c6c89ae0b6a002135f49034b22
Nicholas Esterer <nicholas.esterer@plot.ly>
2020-06-05 12:17:25 -0400

    Centred image

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import skimage.io
 import skimage.transform
 import io_utils
 
-DEFAULT_IMAGE='assets/Euchondrus_septemdentatus_01.JPG'
+DEFAULT_IMAGE = "assets/Euchondrus_septemdentatus_01.JPG"
 
 app = dash.Dash(__name__)
 
@@ -22,49 +22,61 @@ app.layout = html.Div(
                 html.Div(
                     id="banner",
                     children=[
-                        html.H1("Image processing template made with Dash and scikit-image"),
+                        html.H1(
+                            "Image processing template made with Dash and scikit-image"
+                        ),
                     ],
                 ),
-                html.Img(id="image-display",
-                         alt="The transformed image"),
-                html.Div(id='buttons',
-                children=[
-                dcc.Upload(
-                    id="uploader", children=html.Button("Load Image"), multiple=False,
-                    className="inline_button",
-                ),
-                html.A(
-                    id="downloader",
-                    download="image.png",
-                    children=[html.Button("Save Image")],
-                ),
-                ]),
-                html.H6("Rotation (degrees)", id="rotation-display"),
-                dcc.Slider(id="rotation-slider", min=0, max=360, step=0.01, value=0),
-                dcc.Store(id="input-image",
-                data=io_utils.mime_from_img_path(DEFAULT_IMAGE)),
-        html.Div(
-            id="footer",
-            children=[
-                html.P(
+                html.Div(
+                    id="image-container",
                     children=[
-                        "This shows how an app for processing images can be made in ~100 lines of code with Dash and scikit-image. You can add your image processing algorithm by cloning this app: ",
-                        html.A(
-                            "https://github.com/plotly/dash-simple-image-processor",
-                            href="https://github.com/plotly/dash-simple-image-processor",
+                        html.Img(id="image-display", alt="The transformed image"),
+                    ],
+                ),
+                html.Div(
+                    id="buttons",
+                    children=[
+                        dcc.Upload(
+                            id="uploader",
+                            children=html.Button("Load Image"),
+                            multiple=False,
+                            className="inline_button",
                         ),
-                        ". Learn more about ",
-                        html.A("Dash", href="https://plotly.com/dash/"),
-                        " and ",
-                        html.A("scikit-image", href="https://scikit-image.org/"),
-                        ".",
-                    ]
+                        html.A(
+                            id="downloader",
+                            download="image.png",
+                            children=[html.Button("Save Image")],
+                        ),
+                        dcc.Slider(id="rotation-slider", min=0, max=360, step=0.01, value=0),
+                    ],
+                ),
+                html.H6("Rotation (degrees)", id="rotation-display"),
+                dcc.Store(
+                    id="input-image", data=io_utils.mime_from_img_path(DEFAULT_IMAGE)
+                ),
+                html.Div(
+                    id="footer",
+                    children=[
+                        html.P(
+                            children=[
+                                "This shows how an app for processing images can be made in ~100 lines of code with Dash and scikit-image. You can add your image processing algorithm by cloning this app: ",
+                                html.A(
+                                    "https://github.com/plotly/dash-simple-image-processor",
+                                    href="https://github.com/plotly/dash-simple-image-processor",
+                                ),
+                                ". Learn more about ",
+                                html.A("Dash", href="https://plotly.com/dash/"),
+                                " and ",
+                                html.A(
+                                    "scikit-image", href="https://scikit-image.org/"
+                                ),
+                                ".",
+                            ]
+                        ),
+                    ],
                 ),
             ],
         ),
-            ],
-        ),
-
     ],
 )
 
@@ -94,7 +106,7 @@ def process_image(input_image_data, rotation_slider_value):
     im = skimage.transform.rotate(im, rotation_slider_value, resize=True)
 
     mimestr = io_utils.mime_from_img(im)
-    return (mimestr,  mimestr, "Rotation: %.2f\u00B0" % rotation_slider_value)
+    return (mimestr, mimestr, "Rotation: %.2f\u00B0" % rotation_slider_value)
 
 
 if __name__ == "__main__":
